0b62a1c92527d7eb80433d9b835c54ea52907806
Nikolai Gulatz <nikolai@instaffo.de>
2018-03-26 20:25:57 +0200

    Initial commit

---
+++ .config/polybar/scripts/gitlab.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+import os
+import requests
+
+access_token = os.environ.get('GITLAB_PRIVATE_KEY', '')
+
+mr_params = {
+	'scope': 'assigned-to-me',
+	'state': 'opened',
+	'view': 'simple',
+	'per_page': 20
+}
+
+issue_params = {
+	'scope': 'assigned-to-me',
+	'state': 'opened',
+	'per_page': 40
+}
+
+todo_params = {
+	'state': 'pending',
+	'per_page': '40'
+}
+
+auth = {
+	'private_token': access_token
+}
+
+def get_data(endpoint, params):
+	params.update(auth)
+
+	return str(
+		len(
+			requests.get(
+				f'https://gitlab.com/api/v4/{endpoint}',
+				params = params
+			).json()
+		)
+	)
+
+
+merge_requests = get_data('merge_requests', mr_params)
+issues = get_data('issues', issue_params)
+todos = get_data('todos', todo_params)
+
+print(' | '.join([issues, merge_requests, todos]))
---
+++ .zshrc
@@ -0,0 +1,31 @@
+export EDITOR="/usr/bin/subl"
+export PATH="$HOME/.rbenv/bin:$PATH"
+
+eval "$(rbenv init -)"
+
+source $HOME/.antigen.zsh
+
+antigen use oh-my-zsh
+
+antigen bundle extract
+antigen bundle git
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle command-not-found
+
+antigen theme gentoo
+
+antigen apply
+
+# random aliases
+alias ezsh='$EDITOR ~/.zshrc'
+alias szsh='source ~/.zshrc'
+alias sodomize='cowsay -f sodomize'
+alias pw='password-generator -l 24 -c'
+
+# git aliases
+alias gaa='git add --all'
+alias gcm='git commit -m'
+alias gpo='git push origin'
+
+# https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/
+alias config='/usr/bin/git --git-dir=/home/nikolai/.cfg/ --work-tree=/home/nikolai'
